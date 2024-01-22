92318ef839c80ebf6996761920d60783f3ac94e1
aashish124 <aashishahlawat123@gmail.com>
2024-01-23 02:03:53 +0530

    ProtocolAndDelegatesDataPassingDemo Project Completed

---
+++ ProtocolAndDelegatesDataPassingDemo/FirstViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  ProtocolAndDelegatesDataPassingDemo
+//
+//  Created by Aashish Ahlawat on 31/07/23.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController, myDataSendingDelegateProtocol {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+        configureLabels()
+    }
+
+    @IBOutlet weak var nameLabel: UILabel!
+    var holaNameLabel : UILabel!
+    
+    func sendData(data: String) {
+        self.nameLabel.text = data
+    }
+    
+    @IBAction func getDataButton(_ sender: Any) {
+        if let secondVc = storyboard?.instantiateViewController(identifier: "secondVC") as? SecondViewController{
+            secondVc.delegate = self
+            present(secondVc, animated: true)
+        }
+    }
+    
+    
+    private func configureLabels() {
+        nameLabel.font                      = UIFont.boldSystemFont(ofSize: 20)
+        nameLabel.textColor                 = .label
+        nameLabel.adjustsFontSizeToFitWidth = true
+        nameLabel.minimumScaleFactor        = 0.9
+        
+        nameLabel.backgroundColor           = .systemTeal
+        nameLabel.layer.cornerRadius        = 8.0
+        nameLabel.layer.masksToBounds       = true
+        nameLabel.layer.borderColor         = UIColor.systemGray.cgColor
+        
+        nameLabel.lineBreakMode             = .byTruncatingTail
+        nameLabel.textAlignment             = .center
+        
+        nameLabel.text                      = "Wanna see magic? Press Enter."
+    }
+}
+
---
+++ ProtocolAndDelegatesDataPassingDemo/SecondViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SecondViewController.swift
+//  ProtocolAndDelegatesDataPassingDemo
+//
+//  Created by Aashish Ahlawat on 31/07/23.
+//
+
+import UIKit
+
+protocol myDataSendingDelegateProtocol {
+    func sendData(data : String)
+}
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+        configuretextField()
+    }
+    
+    var delegate : myDataSendingDelegateProtocol?
+
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func goBackButton(_ sender: Any) {
+        if self.delegate != nil && self.textField.text != nil {
+            let dataToBeSent = textField.text
+            self.delegate?.sendData(data: dataToBeSent!)
+            dismiss(animated: true,completion: nil)
+        }
+    }
+    
+    
+    func configuretextField() {
+        textField.layer.cornerRadius    = 8.0
+        textField.layer.masksToBounds   = true
+        textField.layer.borderColor     = UIColor.systemGray.cgColor
+    }
+}
